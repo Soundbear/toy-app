f39bebbe7db9af7419e7eb68bfe9d465b330e11d
Addison Jones <addison.jones3@gmail.com>
2018-03-28 20:43:33 +0000

    Toy Heroku Fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,7 +66,7 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
-      @firstMicropost = Micropost.first
+      @firstMicropost = Micropost.find_by(user_id: @user.id)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
